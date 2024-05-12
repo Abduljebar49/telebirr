51b8f04262107eb6cfb489fd864610a234cb770e
Abduljebar <Abduljebar49@gmail.com>
2024-05-12 22:26:25 +0300

    types.d.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,67 @@
 import * as crypto from "crypto";
 import NodeRSA from "node-rsa";
 import axios from "axios";
-import {
-  TelebirrOptions,
-  PaymentOptions,
-  PaymentResponse,
-} from "../types/types";
 
 const TELEBIRR_H5_URL =
   "https://app.ethiomobilemoney.et:2121/ammapi/payment/service-openup/toTradeWebPay";
 const TELEBIRR_IN_APP_URL =
   "https://app.ethiomobilemoney.et:2121/ammapi/payment/service-openup/toTradeMobilePay";
+
+
+  type PaymentMethod = 'web' | 'app';
+
+
+export interface TelebirrOptions {
+    appId: string;
+    appKey: string;
+    shortCode: string;
+    publicKey: string;
+}
+
+export interface PaymentOptions {
+    paymentMethod?: 'web' | 'app';
+    nonce: string;
+    notifyUrl: string;
+    totalAmount: number;
+    outTradeNo: string;
+    receiveName: string;
+    returnApp?: string;
+    returnUrl: string;
+    subject: string;
+    timeoutExpress?: string;
+}
+
+export interface PaymentResponse {
+    success: boolean;
+    response?: any;
+    error?: any;
+}
+export interface PaymentParams {
+    paymentMethod?: PaymentMethod;
+    nonce: string;
+    notifyUrl: string;
+    totalAmount: number;
+    outTradeNo: string;
+    receiveName: string;
+    returnApp?: string;
+    returnUrl: string;
+    subject: string;
+    timeoutExpress?: string;
+}
+
+export interface TelebirrConfig {
+    appId: string;
+    appKey: string;
+    shortCode: string;
+    publicKey: string;
+}
+
+export interface TelebirrResponse {
+    success: boolean;
+    response?: any;
+    error?: any;
+}
+
 
 export class Telebirr {
   private appId: string;
